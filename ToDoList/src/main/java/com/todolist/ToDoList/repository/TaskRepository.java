d8d6a7803898c6752ccd016d06e90d2b49071fcb
miragaia <miguelmiragaia2003@gmail.com>
2024-10-14 18:03:56 +0100

    feat: EIT-25 Repositories Creation

--- ToDoList/src/main/java/com/todolist/ToDoList/repository/TaskRepository.java
+++ ToDoList/src/main/java/com/todolist/ToDoList/repository/TaskRepository.java
@@ -5,9 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    List<Task> findByCompleted(Boolean completed);
-    List<Task> findByPriority(String priority);
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    List<Task> findByUserId(UUID userId);
+    List<Task> findByUserIdAndCategoryId(UUID userId, UUID categoryId);
+    List<Task> findByUserIdAndStatus(UUID userId, String status);
+    List<Task> findByUserIdAndPriority(UUID userId, String priority);
+    List<Task> findByUserIdAndStatusAndPriority(UUID userId, String status, String priority);
+    List<Task> findByUserIdAndCategoryIdAndStatus(UUID userId, UUID categoryId, String status);
+    List<Task> findByUserIdAndCategoryIdAndPriority(UUID userId, UUID categoryId, String priority);
 }
+
